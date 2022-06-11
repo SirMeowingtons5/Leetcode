a51691bac19b5be71cd834a66ba8393fa2fb3ecc
SirMeowingtons5 <34872618+sirmeowingtons5@users.noreply.github.com>
2022-06-11 21:39:02 +0300

    REF: 1. Two Sum https://leetcode.com/problems/two-sum/

--- src/main/kotlin/easy/TwoSum.kt
+++ src/main/kotlin/easy/TwoSum.kt
@@ -2,6 +2,7 @@ package easy
 
 
 /**
+ * 1. Two Sum
  * https://leetcode.com/problems/two-sum/
  */
 
@@ -9,9 +10,25 @@ class TwoSum {
 
     fun twoSum(nums: IntArray, target: Int): IntArray {
 
-        nums.forEachIndexed { index, item ->
-            for (secondIndex in index + 1 until nums.size) {
-                if (nums[index] + nums[secondIndex] == target) return intArrayOf(index, secondIndex)
+        val set = LinkedHashMap<Int, Int>()
+
+        nums.forEach { num ->
+            if (set.contains(num)) {
+                set[num] = 2
+            } else {
+                set[num] = 1
+            }
+        }
+
+        set.keys.forEach { number ->
+            val diff = target - number
+            when {
+                diff == number && set[number] == 2 -> return intArrayOf(
+                    nums.indexOfFirst { it == number },
+                    nums.indexOfLast { it == number },
+                )
+                diff != number && set.contains(diff) -> return intArrayOf(nums.indexOf(number), nums.indexOf(diff)).sortedArray()
+                else -> Unit
             }
         }
 
